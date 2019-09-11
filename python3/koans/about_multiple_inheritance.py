a347a38079fe1920b67f4e8b7fbc3bdff51fea8c
Simon <simon@ferdiesfoodlab.co.uk>
2019-09-11 23:38:19 +0100

    L28 classes multiple inheritance - mro

--- python3/koans/about_multiple_inheritance.py
+++ python3/koans/about_multiple_inheritance.py
@@ -33,7 +33,7 @@ class AboutMultipleInheritance(Koan):
             super().__init__()
             self._name = "Jasper"
 
-        @property
+        @property  # read only property: https://stackoverflow.com/questions/17330160/how-does-the-property-decorator-work
         def name(self):
             return self._name
 
@@ -87,7 +87,7 @@ class AboutMultipleInheritance(Koan):
 
     def test_normal_methods_are_available_in_the_object(self):
         jeff = self.Spiderpig()
-        self.assertRegex(jeff.speak(), __)
+        self.assertRegex(jeff.speak(), "This looks like a job for Spiderpig!")
 
     def test_base_class_methods_are_also_available_in_the_object(self):
         jeff = self.Spiderpig()
@@ -95,22 +95,33 @@ class AboutMultipleInheritance(Koan):
             jeff.set_name("Rover")
         except:
             self.fail("This should not happen")
-        self.assertEqual(__, jeff.can_climb_walls())
+        
+        print(jeff._name) # Rover
+        print(self.Spiderpig.mro())
+        # [<class 'koans.about_multiple_inheritance.AboutMultipleInheritance.Spiderpig'>,
+        # <class 'koans.about_multiple_inheritance.AboutMultipleInheritance.Pig'>,          # no can_climb_walls() method
+        # <class 'koans.about_multiple_inheritance.AboutMultipleInheritance.Spider'>,       # can_climb_walls() => True
+        # <class 'koans.about_multiple_inheritance.AboutMultipleInheritance.Animal'>,
+        # <class 'koans.about_multiple_inheritance.AboutMultipleInheritance.Nameable'>,
+        # <class 'object'>]
+        print(self.Spiderpig.mro()[0].__name__)
+        
+        self.assertEqual(True, jeff.can_climb_walls())
 
     def test_base_class_methods_can_affect_instance_variables_in_the_object(self):
         jeff = self.Spiderpig()
-        self.assertEqual(__, jeff.name)
+        self.assertEqual("Jeff", jeff.name)
 
         jeff.set_name("Rover")
-        self.assertEqual(__, jeff.name)
+        self.assertEqual("Rover", jeff.name)
 
     def test_left_hand_side_inheritance_tends_to_be_higher_priority(self):
         jeff = self.Spiderpig()
-        self.assertEqual(__, jeff.color())
+        self.assertEqual('pink', jeff.color())      # Pig is first class with this method
 
     def test_super_class_methods_are_higher_priority_than_super_super_classes(self):
         jeff = self.Spiderpig()
-        self.assertEqual(__, jeff.legs())
+        self.assertEqual(8, jeff.legs())            # Spider first class with this method
 
     def test_we_can_inspect_the_method_resolution_order(self):
         #
@@ -119,10 +130,10 @@ class AboutMultipleInheritance(Koan):
         mro = type(self.Spiderpig()).mro()
         self.assertEqual('Spiderpig', mro[0].__name__)
         self.assertEqual('Pig', mro[1].__name__)
-        self.assertEqual(__, mro[2].__name__)
-        self.assertEqual(__, mro[3].__name__)
-        self.assertEqual(__, mro[4].__name__)
-        self.assertEqual(__, mro[5].__name__)
+        self.assertEqual('Spider', mro[2].__name__)
+        self.assertEqual('Animal', mro[3].__name__)
+        self.assertEqual('Nameable', mro[4].__name__)
+        self.assertEqual('object', mro[5].__name__)
 
     def test_confirm_the_mro_controls_the_calling_order(self):
         jeff = self.Spiderpig()
@@ -132,7 +143,7 @@ class AboutMultipleInheritance(Koan):
         self.assertRegex(next.here(), 'Pig')
 
         next = super(AboutMultipleInheritance.Pig, jeff)
-        self.assertRegex(next.here(), __)
+        self.assertRegex(next.here(), 'Spider')
 
         # Hang on a minute?!? That last class name might be a super class of
         # the 'jeff' object, but its hardly a superclass of Pig, is it?
